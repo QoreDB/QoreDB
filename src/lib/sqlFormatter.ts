6114390f86d5c6679cba276952704da752eecf93
raphaeltkorp <raphael@tkorp.com>
2026-02-14 22:13:34 +0100

    feat: add Redis support to COLUMN_TYPES and SQL formatter

--- src/lib/sqlFormatter.ts
+++ src/lib/sqlFormatter.ts
@@ -5,6 +5,7 @@ const DIALECT_MAP: Record<Driver, SqlLanguage> = {
   [Driver.Postgres]: 'postgresql',
   [Driver.Mysql]: 'mysql',
   [Driver.Mongodb]: 'sql',
+  [Driver.Redis]: 'sql',
   [Driver.Sqlite]: 'sqlite',
 };
 
